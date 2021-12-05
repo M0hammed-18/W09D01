6f1d4d967367df33cc63f7449078707da3b8c050
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-05 16:23:07 +0300

    LoginDone

--- task/src/component/Login/index.js
+++ task/src/component/Login/index.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import axios from "axios";
+import { useState } from "react";
 
 const Login = () => {
+ const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    try {
+      const result = await axios.post(`${BASE_URL}/login`, {
+        email,
+        password
+      });
+      console.log(result.data.token);
+    } catch (err) {
+      console.log(err);
+    }
+  };
     return (
         <div>
             <>hi login </>
+            <input
+        type="email"
+        name="email"
+        placeholder="enter email "
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="enter password "
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button onClick={login}>Login</button>
         </div>
     )
 }
--- task/src/component/Register/index.js
+++ task/src/component/Register/index.js
@@ -3,36 +3,43 @@ import axios from "axios";
 import { useState } from "react";
 
 const Register = () => {
-
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
 
   const singup = async () => {
-      try{
-    console.log("lll");
-    const result = await axios.post("http://localhost:4000/regester",{
+    try {
+      const result = await axios.post(`${BASE_URL}/regester`, {
         email,
-        password, 
-        role:"61a48ba866acf4f8462bf345"
-       
-    });
-    console.log(result);
-}catch (err){
-    console.log(err);
-}
+        password,
+        role: "61a48ba866acf4f8462bf345",
+      });
+      console.log(result.data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <>
-      
-        <input  type="email" name="email" placeholder="enter email " onChange={(e)=>{setEmail(e.target.value)
-        }} />
-        <input type="password" name="password" placeholder="enter password " onChange={(e)=>{
-            setPassword(e.target.value) }} />
-        <button onClick={singup}>Regester</button>
-      
-
+      <input
+        type="email"
+        name="email"
+        placeholder="enter email "
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="enter password "
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button onClick={singup}>Regester</button>
     </>
   );
 };
